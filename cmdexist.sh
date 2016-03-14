734a3a733a24a093fda0fbfa5b4305952c88d2d2
Onorio Catenacci <ocatenacci@riis.com>
2016-03-14 10:35:24 -0400

    Modifying the cmdexist shell script to have an actual command to check for

--- cmdexist.sh
+++ cmdexist.sh
@@ -3,4 +3,4 @@
 # Shell script to ascertain whether or not a particular command can be found or not.
 # Onorio Catenacci 14 March 2016
 
-command -v foo >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+command -v ${cmd_to_verify} >/dev/null 2>&1 || { echo >&2 "I require ${cmd_to_verify} but it's not installed.  Aborting."; exit 1; }
