15ab133c12c5912afde3febdf0a76824dc51d7cf
Onorio Catenacci <ocatenacci@riis.com>
2016-03-14 10:11:33 -0400

    Adding documentation to script
    Added some initial documentation to the command exists script as well as
    adding the bang command to insure the right shell runs the script.

--- cmdexist.sh
+++ cmdexist.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# cmdexist.sh
+# Shell script to ascertain whether or not a particular command can be found or not.
+# Onorio Catenacci 14 March 2016
+
 command -v foo >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
